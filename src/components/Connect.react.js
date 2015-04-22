f506e480297c80c89db83dd1cdef44e93a3ae2ef
Sean Li <mail@shang.li>
2015-04-22 13:43:11 -0700

    Hooked up form to hub v2 API.

--- src/components/Connect.react.js
+++ src/components/Connect.react.js
@@ -1,9 +1,30 @@
+var $ = require('jquery');
+var _ = require('underscore');
 var React = require('react/addons');
 var Router = require('react-router');
 var RetinaImage = require('react-retina-image');
 var Header = require('./Header.react');
 var metrics = require('../utils/MetricsUtil');
 var util = require('../utils/Util');
+
+var showFormErrors = function ($form, errors) {
+  for (var name in errors) {
+    if (errors.hasOwnProperty(name)) {
+      var fieldError = errors[name][0];
+      var queryName = 'input[name="' + name + '"]';
+      var $field = $form.find(queryName);
+      $field.addClass('error');
+      var $errMsg = $field.next();
+      $errMsg.html(fieldError);
+      $errMsg.show();
+    }
+  }
+};
+
+var clearFormErrors = function ($form) {
+  $form.find('input').removeClass('error');
+  $form.find('.error-message').hide();
+};
 
 var Connect = React.createClass({
   mixins: [ Router.Navigation ],
@@ -44,16 +65,34 @@ var Connect = React.createClass({
   },
   handleSubmitSignUpForm: function (e) {
     e.preventDefault();
-    console.log('SUBMITTED');
+    var $form = $('.form-signup');
+    var data = $form.serialize();
+    clearFormErrors($form);
+    $.ajax({
+      data: data,
+      type: 'POST',
+      url: 'https://hub.dev.docker.com/v2/users/easy_signup/',
+      success: function (response) {
+        console.log(response);
+      },
+      error: function (response) {
+        var errors = response.responseJSON;
+        console.log(errors);
+        showFormErrors($form, errors);
+      }
+    });
   },
   render: function () {
     var userForm;
     if (this.state.page === 'signup') {
       userForm = (
         <form className="form-signup" method="post" onSubmit={this.handleSubmitSignUpForm}>
-          <input ref="usernameInput" maxlength="30" name="username" placeholder="Username" type="text"/>
-          <input name="email" placeholder="E-mail" type="text"/>
-          <input name="password" placeholder="Password" type="password"/>
+          <input ref="usernameInput" maxLength="30" name="username" placeholder="Username" type="text"/>
+          <p className="error-message">Error message.</p>
+          <input ref="emailInput" name="email" placeholder="E-mail" type="text"/>
+          <p className="error-message">Error message.</p>
+          <input ref="passwordInput" name="password" placeholder="Password" type="password"/>
+          <p className="error-message">Error message.</p>
           <button className="btn btn-action" type="submit">Sign Up</button>
           <span className="extra">Already have an account? <a className="btn btn-action" onClick={this.handleGoToLogin}>Login</a></span>
         </form>
@@ -62,7 +101,9 @@ var Connect = React.createClass({
       userForm = (
         <form className="form-login" method="post">
           <input ref="usernameInput" name="username" placeholder="Username" type="text"/>
-          <input name="password" placeholder="Password" type="password"/>
+          <p className="error-message">Error message.</p>
+          <input ref="passwordInput" name="password" placeholder="Password" type="password"/>
+          <p className="error-message">Error message.</p>
           <a className="link" onClick={this.handleForgotPassword}>Forgot your password?</a>
           <button className="btn btn-action" type="submit">Login</button>
           <span className="extra">Don&#39;t have an account yet? <a className="btn btn-action" onClick={this.handleGoToSignUp}>Sign Up</a></span>
